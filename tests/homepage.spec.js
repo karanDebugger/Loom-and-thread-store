8c01109625c1a9c33cb187bad45a50398473acaa
Karan Kaushik <karankaushik504@gmail.com>
2026-07-23 17:27:45 +0530

    Complete Playwright homepage tests

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,23 +1,28 @@
 import { test, expect } from "@playwright/test";
 
 test("Homepage UI renders correctly", async ({ page }) => {
-
   await page.goto("http://localhost:5173");
 
+  // Hero Heading
   await expect(
-    page.getByRole("heading", { name: /Luxury/i })
+    page.getByRole("heading", {
+      level: 1,
+      name: "Luxury Redefined.",
+    })
   ).toBeVisible();
 
+  // Hero Image
   await expect(
     page.getByAltText("Luxury Fashion")
   ).toBeVisible();
 
+  // Shop Now Button
   await expect(
     page.getByTestId("shop-now-btn")
   ).toBeVisible();
 
+  // Featured Products Heading
   await expect(
     page.getByTestId("featured-products-heading")
   ).toBeVisible();
-
 });
